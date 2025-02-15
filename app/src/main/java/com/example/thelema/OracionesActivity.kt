4159e353f71218ee659b682bb7d64f13f90e30a5
haru9393 <jm.nuitlux@gmail.com>
2025-02-14 21:44:16 -0300

    cambios 5

--- app/src/main/java/com/example/thelema/OracionesActivity.kt
+++ app/src/main/java/com/example/thelema/OracionesActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.glance.visibility
 
 class OracionesActivity : AppCompatActivity() {
 
