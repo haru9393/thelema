34b21f92bc405a6b9ad888d1dc41adcc2a9be455
haru9393 <jm.nuitlux@gmail.com>
2025-02-15 11:37:36 -0300

    cambios 15 CON ERRORES

--- app/src/main/java/com/example/thelema/MainActivity.kt
+++ app/src/main/java/com/example/thelema/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.thelema
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.view.View
 import android.widget.ScrollView
 import android.text.method.ScrollingMovementMethod
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // Datos de contenido de los libros
     private val bookContentData: Map<String, Map<String, Map<String, String>>> = mapOf(
         "Liber AL vel Legis" to mapOf(
             "Capítulo 1" to mapOf(
@@ -109,129 +110,142 @@ class MainActivity : AppCompatActivity() {
 
 
     // Lista de oraciones y rituales
+    // Lista de oraciones y rituales
     private val oraciones = listOf(
         Pair("Oración a Nuit", """
-    Oh Nuit, vasto y eterno misterio,  
-    Madre cósmica que envuelves al universo entero,  
-    Tú que eres la estrella que no se apaga,  
-    Contigo, me uno en la luz infinita y el amor absoluto.  
-    Eres el espacio y el tiempo,  
-    Y en tu abrazo encuentro la libertad y el éxtasis eterno.  
-    Oh Nuit, guíame en tu silencio y en tu sabiduría,  
-    Para que mi voluntad se disuelva en tu voluntad infinita.
+        Oh Nuit, vasto y eterno misterio,  
+        Madre cósmica que envuelves al universo entero,  
+        Tú que eres la estrella que no se apaga,  
+        Contigo, me uno en la luz infinita y el amor absoluto.  
+        Eres el espacio y el tiempo,  
+        Y en tu abrazo encuentro la libertad y el éxtasis eterno.  
+        Oh Nuit, guíame en tu silencio y en tu sabiduría,  
+        Para que mi voluntad se disuelva en tu voluntad infinita.
     """.trimIndent()),
         Pair("Oración a Hadit", """
-    Oh Hadit, el centro sin círculo,  
-    La llama eterna que arde en lo más profundo de mi ser,  
-    Tú que eres la chispa de la voluntad,  
-    Avivando mi fuego interior con tu pasión infinita.  
-    Dame el poder de ser uno con el movimiento,  
-    Haz de mí un vehículo para la acción pura,  
-    Mi voluntad es tu voluntad,  
-    Y a través de ella, manifiesto mi verdadero ser en el mundo.
+        Oh Hadit, el centro sin círculo,  
+        La llama eterna que arde en lo más profundo de mi ser,  
+        Tú que eres la chispa de la voluntad,  
+        Avivando mi fuego interior con tu pasión infinita.  
+        Dame el poder de ser uno con el movimiento,  
+        Haz de mí un vehículo para la acción pura,  
+        Mi voluntad es tu voluntad,  
+        Y a través de ella, manifiesto mi verdadero ser en el mundo.
     """.trimIndent()),
         Pair("Oración a Ra-Hoor-Khuit", """
-    Oh Ra-Hoor-Khuit, Señor de la guerra y la justicia,  
-    Tú que traes la luz y la ley a la oscuridad,  
-    Te invoco en tu forma guerrera y sublime.  
-    Que tu fuerza guíe mi voluntad en este día,  
-    Y que tu espada, afilada y brillante, corte cualquier obstáculo en mi camino.  
-    Que tu presencia sea mi protección,  
-    Y que tu luz me inspire a luchar por lo que es justo.
+        Oh Ra-Hoor-Khuit, Señor de la guerra y la justicia,  
+        Tú que traes la luz y la ley a la oscuridad,  
+        Te invoco en tu forma guerrera y sublime.  
+        Que tu fuerza guíe mi voluntad en este día,  
+        Y que tu espada, afilada y brillante, corte cualquier obstáculo en mi camino.  
+        Que tu presencia sea mi protección,  
+        Y que tu luz me inspire a luchar por lo que es justo.
     """.trimIndent()),
         Pair("Oración a Hoor-paar-kraat", """
-    Oh Hoor-paar-kraat, señor del silencio y la paz interior,  
-    En tu quietud encuentro la verdad,  
-    En tu misterio, la llave de mi alma.  
-    Que tu silencio me enseñe a escuchar,  
-    Que tu sabiduría ilumine mi mente.  
-    Permíteme descansar en la calma de tu presencia,  
-    Para que, en la quietud, pueda descubrir mi verdadera Voluntad.
+        Oh Hoor-paar-kraat, señor del silencio y la paz interior,  
+        En tu quietud encuentro la verdad,  
+        En tu misterio, la llave de mi alma.  
+        Que tu silencio me enseñe a escuchar,  
+        Que tu sabiduría ilumine mi mente.  
+        Permíteme descansar en la calma de tu presencia,  
+        Para que, en la quietud, pueda descubrir mi verdadera Voluntad.
     """.trimIndent()),
         Pair("Oración a Babalon", """
-    Oh Babalon, madre del éxtasis y del fuego sagrado,  
-    Tú que recibes con amor y entregas con pasión,  
-    Derrama sobre mí tu fuerza y tu deseo infinito.  
-    Que mi copa rebose con tu vino sagrado,  
-    Que mi espíritu se libere en la entrega total.  
-    Oh gran Babalon, enséñame el camino de la pasión y la libertad.
+        Oh Babalon, madre del éxtasis y del fuego sagrado,  
+        Tú que recibes con amor y entregas con pasión,  
+        Derrama sobre mí tu fuerza y tu deseo infinito.  
+        Que mi copa rebose con tu vino sagrado,  
+        Que mi espíritu se libere en la entrega total.  
+        Oh gran Babalon, enséñame el camino de la pasión y la libertad.
     """.trimIndent()),
         Pair("Oración a Therion", """
-    Oh Therion, bestia indómita del poder y la voluntad,  
-    Tú que ruges con la fuerza del universo,  
-    Despierta en mí el coraje de seguir mi camino.  
-    Que tu fuego arda en mi sangre,  
-    Que tu furia sea mi escudo,  
-    Y que mi voluntad sea firme como la tuya.  
-    En tu fuerza, encuentro mi libertad.
+        Oh Therion, bestia indómita del poder y la voluntad,  
+        Tú que ruges con la fuerza del universo,  
+        Despierta en mí el coraje de seguir mi camino.  
+        Que tu fuego arda en mi sangre,  
+        Que tu furia sea mi escudo,  
+        Y que mi voluntad sea firme como la tuya.  
+        En tu fuerza, encuentro mi libertad.
     """.trimIndent()),
         Pair("Ritual de la Estrella Rubí", """
-    1. De pie, de cara al Este.
-    2. Trazar el pentagrama: usando un dedo o una espada ritual, dibuja un pentagrama en el aire con la punta hacia el Este. Enfócate en la energía del pentagrama.
-    3. Invocar las energías elementales: Comienza por invocar las energías de los 4 elementos, comenzando por el Norte (tierra), luego el Este (aire), el Sur (fuego) y el Oeste (agua). Puedes decir: 
-    "Yo invoco el poder del Norte, del Sur, del Este y del Oeste. Que los elementos me guíen y protejan."
-    4. Cerrar el círculo: Visualiza una barrera de luz dorada o blanca que rodea el espacio donde estás. Mientras lo haces, recita las palabras de protección: 
-    "Que este círculo esté cerrado en todas las direcciones. Que esté protegido por la energía divina."
-    5. Recita la oración de invocación: Aquí puedes recitar una invocación a los dioses, a la fuerza universal o cualquier oración de tu preferencia.
-    6. Finaliza el ritual con la consagración del espacio: "Que este espacio quede consagrado y protegido por las fuerzas cósmicas."
-    7. Cerrar el círculo: Para finalizar, visualiza la luz protectora desapareciendo, dejando el espacio en calma.
+        1. De pie, de cara al Este.
+        2. Trazar el pentagrama: usando un dedo o una espada ritual, dibuja un pentagrama en el aire con la punta hacia el Este. Enfócate en la energía del pentagrama.
+        3. Invocar las energías elementales: Comienza por invocar las energías de los 4 elementos, comenzando por el Norte (tierra), luego el Este (aire), el Sur (fuego) y el Oeste (agua). Puedes decir: 
+        "Yo invoco el poder del Norte, del Sur, del Este y del Oeste. Que los elementos me guíen y protejan."
+        4. Cerrar el círculo: Visualiza una barrera de luz dorada o blanca que rodea el espacio donde estás. Mientras lo haces, recita las palabras de protección: 
+        "Que este círculo esté cerrado en todas las direcciones. Que esté protegido por la energía divina."
+        5. Recita la oración de invocación: Aquí puedes recitar una invocación a los dioses, a la fuerza universal o cualquier oración de tu preferencia.
+        6. Finaliza el ritual con la consagración del espacio: "Que este espacio quede consagrado y protegido por las fuerzas cósmicas."
+        7. Cerrar el círculo: Para finalizar, visualiza la luz protectora desapareciendo, dejando el espacio en calma.
     """.trimIndent()),
         Pair("Liber Resh", """
-    1. Saludar al Sol en cada uno de sus aspectos: Mañana, mediodía, atardecer y medianoche.
-    2. Realizar el saludo: Levanta las manos hacia el Sol mientras recitas las palabras sagradas de cada fase. Por ejemplo, al amanecer, recita:
-    "Te saludo, oh Sol, gran luz de sabiduría, al comienzo del día."
-    3. Recitar las palabras sagradas: Durante cada saludo, recita: "¡Que la Luz del Sol ilumine mi camino y me otorgue sabiduría y fortaleza!"
-    4. Finaliza el ritual agradeciendo al Sol y visualiza cómo la luz de la energía solar te llena de poder.
+        1. Saludar al Sol en cada uno de sus aspectos: Mañana, mediodía, atardecer y medianoche.
+        2. Realizar el saludo: Levanta las manos hacia el Sol mientras recitas las palabras sagradas de cada fase. Por ejemplo, al amanecer, recita:
+        "Te saludo, oh Sol, gran luz de sabiduría, al comienzo del día."
+        3. Recitar las palabras sagradas: Durante cada saludo, recita: "¡Que la Luz del Sol ilumine mi camino y me otorgue sabiduría y fortaleza!"
+        4. Finaliza el ritual agradeciendo al Sol y visualiza cómo la luz de la energía solar te llena de poder.
     """.trimIndent()),
         Pair("Ritual del Safiro", """
-    1. Preparar el altar: Coloca símbolos de protección como una vela, incienso, un cáliz o cualquier objeto que represente la energía de protección.
-    2. Invocar al Ángel Guardián: Recita las palabras de invocación al Ángel Guardián, como por ejemplo:
-    "Ángel Guardián, protector de mi alma, ven a mí en este ritual y guíame."
-    3. Recitar las palabras de consagración del Safiro: “Que el poder del Safiro me rodee, que la energía azul me proteja y me otorgue claridad espiritual.”
-    4. Visualizar el aura del Safiro: Mientras recitas, imagina una luz azul que rodea tu cuerpo, simbolizando protección y sabiduría.
-    5. Cerrar el ritual: Para finalizar, recita una oración de gratitud y protección: 
-    "Gracias, Ángel Guardián, por tu protección. Que este ritual se cierre con bendiciones y claridad."
+        1. Preparar el altar: Coloca símbolos de protección como una vela, incienso, un cáliz o cualquier objeto que represente la energía de protección.
+        2. Invocar al Ángel Guardián: Recita las palabras de invocación al Ángel Guardián, como por ejemplo:
+        "Ángel Guardián, protector de mi alma, ven a mí en este ritual y guíame."
+        3. Recitar las palabras de consagración del Safiro: “Que el poder del Safiro me rodee, que la energía azul me proteja y me otorgue claridad espiritual.”
+        4. Visualizar el aura del Safiro: Mientras recitas, imagina una luz azul que rodea tu cuerpo, simbolizando protección y sabiduría.
+        5. Cerrar el ritual: Para finalizar, recita una oración de gratitud y protección: 
+        "Gracias, Ángel Guardián, por tu protección. Que este ritual se cierre con bendiciones y claridad."
     """.trimIndent())
     )
 
-
+    // Lista de preguntas frecuentes
+    private val preguntasFrecuentes = listOf(
+        Pair("¿Qué es Thelema?", "Thelema es una filosofía espiritual basada en la ley de la voluntad."),
+        Pair("¿Cómo practicar Thelema?", "La práctica de Thelema implica seguir tu propia voluntad..."),
+        // Otras preguntas...
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Referencias a los botones
+        // Referencias a los botones y vistas
         val buttonLiberAlVelLegis: Button = findViewById(R.id.buttonLiberAlVelLegis)
         val buttonLiberII: Button = findViewById(R.id.buttonLiberII)
         val buttonLiberTzaddi: Button = findViewById(R.id.buttonLiberTzaddi)
         val buttonOraciones: Button = findViewById(R.id.buttonOraciones)
+        val buttonPreguntas: Button = findViewById(R.id.buttonPreguntas)
         val scrollView: ScrollView = findViewById(R.id.scrollView)
         val textView: TextView = findViewById(R.id.textView)
 
         // Habilitar desplazamiento en el TextView
         textView.movementMethod = ScrollingMovementMethod()
 
-        // Mostrar contenido de Liber AL vel Legis
+        // Botón para mostrar contenido de Liber AL vel Legis
         buttonLiberAlVelLegis.setOnClickListener {
             textView.text = getBookContent("Liber AL vel Legis")
             scrollView.visibility = View.VISIBLE
         }
 
-        // Mostrar contenido de Liber II
+        // Botón para mostrar contenido de Liber II
         buttonLiberII.setOnClickListener {
             textView.text = getBookContent("Liber II")
             scrollView.visibility = View.VISIBLE
         }
 
-        // Mostrar contenido de Liber Tzaddi
+        // Botón para mostrar contenido de Liber Tzaddi
         buttonLiberTzaddi.setOnClickListener {
             textView.text = getBookContent("Liber Tzaddi")
             scrollView.visibility = View.VISIBLE
         }
 
-        // Mostrar oraciones y rituales
+        // Botón para mostrar oraciones y rituales
         buttonOraciones.setOnClickListener {
             textView.text = getOracionesContent()
+            scrollView.visibility = View.VISIBLE
+        }
+
+        // Botón para mostrar preguntas frecuentes
+        buttonPreguntas.setOnClickListener {
+            textView.text = getPreguntasFrecuentesContent()
             scrollView.visibility = View.VISIBLE
         }
     }
@@ -256,6 +270,16 @@ class MainActivity : AppCompatActivity() {
         oraciones.forEach { (title, text) ->
             content.append("$title\n")
             content.append("$text\n\n")
+        }
+        return content.toString()
+    }
+
+    // Función para obtener el contenido de las preguntas frecuentes
+    private fun getPreguntasFrecuentesContent(): String {
+        val content = StringBuilder()
+        preguntasFrecuentes.forEach { (question, answer) ->
+            content.append("Pregunta: $question\n")
+            content.append("Respuesta: $answer\n\n")
         }
         return content.toString()
     }
